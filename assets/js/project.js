d690f34ff00f852836938a95c99f527880e9e0af
WCY-dt <834421194@qq.com>
2024-06-03 01:06:18 +0800

    feat: Add project section (not used)

---
+++ assets/js/project.js
@@ -0,0 +1,126 @@
+let dataHTMLs = []
+const container = document.getElementById('project-content');
+let currentDataIndex = 2
+
+function getIndexDelta(idx, delta) {
+    return (idx + delta + totDataNum) % totDataNum
+}
+
+fetch('./assets/data/project.json')
+    .then(response => response.json())
+    .then(datas => {
+        datas.forEach((data, index) => {
+            const div = document.createElement('div')
+            div.classList.add('project-item')
+            div.innerHTML = `
+                <dt class="project-info">
+                    <span>${data.title}</span>
+                </dt>
+                <dd class="project-desc">${data.description}</dd>
+                <menu class="project-link">
+                    ${!data.link ? `` : `
+                        <a href="${data.link}" target="_blank">
+                            <span>Demo</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+                                <path d="M120-120v-720h360v80H200v560h560v-280h80v360H120Zm268-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/>
+                            </svg>
+                        </a>
+                    `}
+                    ${!data.repolink ? `` : `
+                        <a href="${data.repolink}" target="_blank">
+                            <span>GitHub</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
+                                <path fill="currentColor" d="M120-120v-720h360v80H200v560h560v-280h80v360H120Zm268-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/>
+                            </svg>
+                        </a>
+                    `}
+                </menu>
+            `
+            dataHTMLs.push(div)
+        })
+
+        totDataNum = dataHTMLs.length
+        currentDataIndex = 2
+
+        const container = document.getElementById('project-content');
+
+        dataHTMLs.slice(getIndexDelta(currentDataIndex, -2), getIndexDelta(currentDataIndex, 2) + 1).forEach(dataHTML => {
+            container.appendChild(dataHTML)
+        })
+
+        container.scrollTo({
+            left: container.scrollWidth / 2 - container.clientWidth / 2,
+            behavior: 'smooth'
+        })
+    })
+
+function moveRight() {
+    lastElementIdx = getIndexDelta(currentDataIndex, -3)
+    container.insertBefore(dataHTMLs[lastElementIdx], container.children[0])
+    container.removeChild(container.children[container.children.length - 1])
+    currentDataIndex = getIndexDelta(currentDataIndex, -1)
+
+    container.scrollTo({
+        left: container.scrollWidth / 2 - container.clientWidth / 2,
+        behavior: 'smooth'
+    })
+}
+
+function moveLeft() {
+    nextElementIdx = getIndexDelta(currentDataIndex, 3)
+    container.appendChild(dataHTMLs[nextElementIdx])
+    container.removeChild(container.children[0])
+    currentDataIndex = getIndexDelta(currentDataIndex, 1)
+
+    container.scrollTo({
+        left: container.scrollWidth / 2 - container.clientWidth / 2,
+        behavior: 'smooth'
+    })
+}
+
+let isMouseDown = false;
+let isScrolling = false;
+
+container.addEventListener('mouseenter', (e) => {
+    isMouseDown = false;
+    isScrolling = false;
+    container.addEventListener('mousedown', mouseDownHandler);
+    container.addEventListener('mousemove', mouseMoveHandler);
+    container.addEventListener('mouseup', mouseUpHandler);
+})
+
+container.addEventListener('mouseleave', (e) => {
+    container.removeEventListener('mousedown', mouseDownHandler);
+    container.removeEventListener('mousemove', mouseMoveHandler);
+    container.removeEventListener('mouseup', mouseUpHandler);
+    isMouseDown = false;
+    isScrolling = false;
+})
+
+function mouseDownHandler(e) {
+    isMouseDown = true;
+}
+
+function mouseMoveHandler(e){
+    if (isMouseDown && !isScrolling) {
+        isScrolling = true;
+
+        if (e.movementX > 0) {
+            moveRight()
+        } else {
+            moveLeft()
+        }
+
+        setTimeout(() => { isScrolling = false; }, 500);
+    }
+}
+
+function mouseUpHandler(e) {
+    isMouseDown = false;
+}
+
+let leftArrow = document.getElementById('project-left')
+let rightArrow = document.getElementById('project-right')
+
+leftArrow.addEventListener('click', moveRight)
+rightArrow.addEventListener('click', moveLeft)
