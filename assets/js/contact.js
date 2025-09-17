26ff2e621b9f5e60939a7fd978848bca54627486
WCY-dt <834421194@qq.com>
2025-09-17 22:23:18 +0800

    chore: clear

--- assets/js/contact.js
+++
@@ -1,7 +0,0 @@
-// 查找所有的 .contact__detail
-contactDetails = document.querySelectorAll('.contact__detail');
-
-// 每个都将innerhtml都复制多份
-contactDetails.forEach((contactDetail, i) => {
-    contactDetail.innerHTML = contactDetail.innerHTML.repeat(20);
-});
